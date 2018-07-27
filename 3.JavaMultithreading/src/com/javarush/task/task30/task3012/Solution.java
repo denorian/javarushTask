fd5019aa07d9c57d3b076bad13a90300f06235b6
AlexanderBrovko <denorian@yandex.ru>
2018-07-27 21:14:14 +0300

    3012

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -11,6 +11,32 @@ public class Solution {
     }
 
     public void createExpression(int number) {
-        //напишите тут ваш код
+        int[] arr = {1, 3, 9, 27, 81, 243, 729, 2187};
+        String lineNum = "";
+        while (number > 0) {
+            if (number % 3 == 0) {
+                lineNum = lineNum + "0";
+                number /= 3;
+            } else if (number % 3 == 1) {
+                lineNum = lineNum + "+";
+                number /= 3;
+            } else if (number % 3 == 2) {
+                lineNum = lineNum + "-";
+                number /= 3;
+                number++;
+            }
+        }
+        int sum = 0;
+        String str = "";
+        for (int i = 0; i < lineNum.length(); i++) {
+            if (lineNum.charAt(i) == '+') {
+                str += " + " + arr[i];
+                sum += arr[i];
+            } else if (lineNum.charAt(i) == '-') {
+                str += " - " + arr[i];
+                sum -= arr[i];
+            }
+        }
+        System.out.println(sum + " =" + str);
     }
 }
