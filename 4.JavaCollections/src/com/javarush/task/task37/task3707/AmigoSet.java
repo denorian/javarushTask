efcb5fb58df0820caefd44e35e12942c4503b2f8
AlexanderBrovko <denorian@yandex.ru>
2018-09-01 23:38:52 +0300

    AmigoSet done

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -17,16 +20,68 @@ public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable,
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     public boolean add(Object o) {
         return map.put((E) o,PRESENT) == null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    @Override
+    public Object clone() throws InternalError {
+        try {
+            AmigoSet copy = (AmigoSet)super.clone();
+            copy.map = (HashMap) map.clone();
+            return copy;
+        }catch (Exception e){
+            throw new InternalError();
+        }
+    }
+
+    private void writeObject(ObjectOutputStream oos) throws Exception {
+        oos.defaultWriteObject();
+        oos.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+        oos.writeFloat(HashMapReflectionHelper.callHiddenMethod(map,"loadFactor"));
+        oos.writeInt(map.size());
+        for (E e : map.keySet())
+            oos.writeObject(e);
+    }
+
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        ois.defaultReadObject();
+        int capacity = ois.readInt();
+        float loadFactor = ois.readFloat();
+        int size = ois.readInt();
+        map = new HashMap<>(capacity,loadFactor);
+        for (int i = 0; i < size; i++){
+            E e = (E) ois.readObject();
+            map.put(e,PRESENT);
+        }
     }
 }
