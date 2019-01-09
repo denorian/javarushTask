286d679e60fdce86fa8eff1d860f679d4897299f
AlexanderBrovko <denorian@yandex.ru>
2019-01-10 00:30:32 +0300

    Ресторан(15) has done

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -11,5 +11,12 @@ public class Restaurant {
         tablet.addObserver(cook);
         cook.addObserver(waiter);
         tablet.createOrder();
+
+        DirectorTablet directorTablet = new DirectorTablet();
+        directorTablet.printActiveVideoSet();
+        directorTablet.printAdvertisementProfit();
+        directorTablet.printArchivedVideoSet();
+        directorTablet.printCookWorkloading();
+
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,18 +1,17 @@
 package com.javarush.task.task27.task2712.statistic;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StatisticManager {
 
     private StatisticStorage statisticStorage = new StatisticStorage();
-    
+
     private static StatisticManager ourInstance = new StatisticManager();
+    private Set<Cook> cooks = new HashSet<>();
 
     public static StatisticManager getInstance() {
         return ourInstance;
@@ -21,22 +20,24 @@ public class StatisticManager {
     private StatisticManager() {
     }
 
-    public void register(EventDataRow data){
+    public void register(EventDataRow data) {
         statisticStorage.put(data);
+    }
+
+    public void register(Cook cook) {
+        cooks.add(cook);
     }
 
     private class StatisticStorage {
         Map<EventType, List<EventDataRow>> storage = new HashMap<>();
 
-        private StatisticStorage()
-        {
-            for (EventType eventType : EventType.values())
-            {
+        private StatisticStorage() {
+            for (EventType eventType : EventType.values()) {
                 storage.put(eventType, new ArrayList<EventDataRow>());
             }
         }
 
-        private void put(EventDataRow data){
+        private void put(EventDataRow data) {
             storage.get(data.getType()).add(data);
         }
 
