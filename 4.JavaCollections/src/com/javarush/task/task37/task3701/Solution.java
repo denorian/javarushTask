6e30365cab994321114237cbf4b2b0f4ea7a718d
AlexanderBrovko <denorian@yandex.ru>
2019-01-27 23:22:38 +0300

    task3701 done

--- 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task37.task3701;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /* 
 Круговой итератор
@@ -23,6 +24,30 @@ public class Solution<T> extends ArrayList<T> {
         }
     }
 
-    public class RoundIterator {
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
+    }
+
+    public class RoundIterator implements Iterator<T> {
+        private Iterator<T> iterator = Solution.super.iterator();
+
+        @Override
+        public boolean hasNext() {
+            return size() != 0;
+        }
+
+        @Override
+        public T next() {
+            if(!iterator.hasNext())
+                iterator = Solution.super.iterator();
+
+            return iterator.next();
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+        }
     }
 }
